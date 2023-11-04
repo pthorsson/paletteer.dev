2b90f0fcf7facb6578e89365cffa50ded3bd651f
Patrik Thorsson <patrik@thorsson.dev>
2023-11-04 22:55:25 +0100

    updated footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,12 @@
-<script lang="ts">
-</script>
-
-<footer>footer</footer>
+<footer>
+  <div class="container">
+    <div>
+      Built with <span style="color: salmon;">♥</span> by
+      <a href="https://github.com/pthorsson" target="_blank">@pthorsson</a>
+    </div>
+    <div>v0.1.0</div>
+  </div>
+</footer>
 
 <style>
   footer {
@@ -9,7 +14,16 @@
     color: var(--fg);
     justify-content: center;
     align-items: center;
-    background-color: var(--bg);
-    border-top: 1px solid var(--fg);
+    width: 100%;
+    background-color: var(--component-1);
+    margin-top: var(--base-4);
+  }
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    max-width: var(--max-width);
+    width: 100%;
+    padding: var(--base-4) var(--base-2);
   }
 </style>

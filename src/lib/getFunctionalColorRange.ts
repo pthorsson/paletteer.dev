40b472e546558aa6326a115976cafcb07671ed14
Patrik Thorsson <patrik@thorsson.dev>
2023-11-04 23:09:38 +0100

    adjusted functional color range shades

--- src/lib/getFunctionalColorRange.ts
+++ src/lib/getFunctionalColorRange.ts
@@ -6,17 +6,17 @@ export function getFunctionalColorRange(
   black: string
 ) {
   const shades = [
-    { name: 'w100', value: white, mix: 90 },
+    { name: 'w100', value: white, mix: 85 },
     { name: 'w80', value: white, mix: 70 },
-    { name: 'w60', value: white, mix: 50 },
-    { name: 'w40', value: white, mix: 30 },
-    { name: 'w20', value: white, mix: 10 },
+    { name: 'w60', value: white, mix: 55 },
+    { name: 'w40', value: white, mix: 40 },
+    { name: 'w20', value: white, mix: 20 },
     { name: '0', value: white, mix: 0 },
-    { name: 'b20', value: black, mix: 10 },
-    { name: 'b40', value: black, mix: 30 },
-    { name: 'b60', value: black, mix: 50 },
+    { name: 'b20', value: black, mix: 20 },
+    { name: 'b40', value: black, mix: 40 },
+    { name: 'b60', value: black, mix: 55 },
     { name: 'b80', value: black, mix: 70 },
-    { name: 'b100', value: black, mix: 90 },
+    { name: 'b100', value: black, mix: 85 },
   ];
 
   const keyColorRange = shades.map((shade) => {

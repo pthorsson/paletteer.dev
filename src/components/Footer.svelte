ccfc816358943ca4a9fe09c9dbd99648b164fda5
Patrik Thorsson <patrik@thorsson.dev>
2023-11-05 12:10:16 +0100

    footer alignment fixes

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -38,25 +38,27 @@
   .container {
     display: grid;
     justify-content: space-between;
+    align-items: center;
     grid-template-columns: repeat(3, 1fr);
     max-width: var(--max-width);
     width: 100%;
     padding: var(--base-4) var(--base-2);
   }
 
-  .container > * {
+  .container > *:nth-child(1) {
     display: flex;
-    justify-content: center;
     align-items: center;
-    gap: 0.5em;
-    white-space: nowrap;
-  }
-
-  .container > *:first-child {
     justify-content: flex-start;
   }
 
-  .container > *:last-child {
+  .container > *:nth-child(2) {
+    text-align: center;
+    white-space: nowrap;
+  }
+
+  .container > *:nth-child(3) {
+    display: flex;
+    align-items: center;
     justify-content: flex-end;
   }
 

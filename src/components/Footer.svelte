0d7a39b1aa5d4bbe76bcdd00dff32d742f3074b2
Patrik Thorsson <patrik@thorsson.dev>
2023-11-05 11:40:00 +0100

    updated links in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,20 @@
+<script>
+  import GithubLogo from '$components/GithubLogo.svelte';
+</script>
+
 <footer>
   <div class="container">
     <div>
       Built with <span style="color: salmon;">♥</span> by
       <a href="https://github.com/pthorsson" target="_blank">@pthorsson</a>
     </div>
-    <div>v0.1.0</div>
+    <a
+      href="https://github.com/pthorsson/paletteer.dev"
+      target="_blank"
+      class="icon-link"
+    >
+      <GithubLogo />
+    </a>
   </div>
 </footer>
 
@@ -22,8 +32,24 @@
   .container {
     display: flex;
     justify-content: space-between;
+    align-items: center;
     max-width: var(--max-width);
     width: 100%;
     padding: var(--base-4) var(--base-2);
+  }
+
+  a {
+    color: var(--fg);
+    transition: all 150ms;
+    opacity: 0.7;
+    text-decoration: none;
+  }
+
+  a:hover {
+    opacity: 1;
+  }
+
+  .icon-link {
+    display: block;
   }
 </style>
